54051a44a67008abcb5eb76157da1e2bd11ffc02
THORIQTKJX-A <muhammadthoriqrobbani@gmail.com>
2024-07-30 09:40:40 +0700

    Add files via upload

---
+++ perkenalan.py
@@ -0,0 +1,7 @@
+nama = "M. Thoriq robbani"
+Kelas = "TKJ X A"
+hobi "Maen game"
+
+print("Perkenalkan nama saya", nama)
+print(f"saya kelas {kelas}")
+print(f"hobi saya {hobi}, Saya sangat senang melakukan ny")
