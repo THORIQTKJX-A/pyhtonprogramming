5b3c9d80e101581a02499fc092d3580b6354429d
THORIQTKJX-A <muhammadthoriqrobbani@gmail.com>
2024-08-06 09:43:07 +0700

    Add files via upload

---
+++ membership.py
@@ -0,0 +1,4 @@
+makanan = ["kue pancong", "nasgor", "lotek", "burger"]
+
+print("apakah nasi uduk merupakan anggota?", "nasi uduk" in makanan)
+print("apakah burger merupakan anggota?", "burger" in makanan)
---
+++ nilai.py
@@ -0,0 +1,10 @@
+nilai = 89
+
+if nilai > 90 and nilai <= 100:
+    print("Grade A")
+if nilai > 80 and nilai < 91:
+    print("Grade B")
+if nilai >= 75 and nilai <= 80:
+    print("Grade C") 
+else:
+    print("Grade D")
